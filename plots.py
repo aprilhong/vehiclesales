0e72127e99c006074804b7bf1b12ab995d37b02a
April Hong <78663820+aprilhong@users.noreply.github.com>
2024-06-10 14:28:15 -0400

    finished data cleaning and saved cleaned dataset

--- plots.py
+++ plots.py
@@ -20,7 +20,7 @@ def boxhist(df,x):
     plt.show()
 
 
-def bplts(df,x,y,w,h,xrot,view,lim):
+def bplts(df,x,y,w,h,xrot,orderby,lim):
     
     '''
     Function to plot bar graph for 2 variables
@@ -29,28 +29,27 @@ def bplts(df,x,y,w,h,xrot,view,lim):
         y: y variable
         w: plot width
         h: plot height
-        xrot: xtick rotation
-        order: order graph
-        lim: limit x values
+        xrot: xtick rotation (45, 30, etc.)
+        orderby: display graph ('desc', 'asc')
+        lim: number of x values to show (None, #)
     '''
+
+    if orderby == 'asc':
+        order = df.groupby([x])[y].mean().sort_values(ascending=True).head(lim).index
+    elif orderby == 'desc':
+        order = df.groupby([x])[y].mean().sort_values(ascending=False).head(lim).index
+    else:
+        order = None
+
     # setting the dimensions of the plot
     fig, ax = plt.subplots(figsize=(w, h))
-    sns.barplot(df, x=x, y=y, ax=ax, errorbar=None,color='#b3cede',order=orders)
-
-    if  view == 'lim':
-        orders = df[y].value_counts(ascending=False).head(lim).index
-    elif view == 'asc':
-        orders == df[y].value_counts(ascending=True).index
-    elif  view == 'desc':
-        orders == df[y].value_counts(ascending=False).index
-    else:
-        orders=None
-
+    sns.barplot(df, x=x, y=y, ax=ax, errorbar=None,color='#b3cede',order=order)
     plt.title(f'{y} by {x}', fontsize=12)
     plt.ylabel(y)
     plt.xlabel(x)
     plt.xticks(rotation=xrot, ha='right')
     plt.show()
+
 
 
 def bhplt(df,feat,w,h,wspace,bw): 
@@ -106,7 +105,7 @@ def bhplt(df,feat,w,h,wspace,bw):
     plt.show()
 
 
-def ctplt(df, x:str, w, h, lim,xrot, lbl:str, lbl_type:str):
+def ctplt(df, x, y, w, h, lim,xrot, lbl:str, lbl_type:str):
     '''
     Function to plot count graph
         df: df name
@@ -119,13 +118,26 @@ def ctplt(df, x:str, w, h, lim,xrot, lbl:str, lbl_type:str):
         lbl_type: display as count or percentage ('%','#')
     '''
 
+    if y == None:
+        order=df[x].value_counts(ascending=False).head(lim).index
+    elif x == None:
+        order=df[y].value_counts(ascending=False).head(lim).index
+    else:
+        None
+
     fig = plt.figure(figsize=(w,h))
-    ax = sns.countplot(x=df[x],order=df[x].value_counts(ascending=False).head(lim).index, color='#b3cede')
+    
+    if x == None:
+        ax = sns.countplot(data=df, y=y, order=order, color='#b3cede')
+    else:
+        ax = sns.countplot(data=df, x=x, order=order, color='#b3cede')
+        
     plt.xticks(rotation=xrot)
     ax.tick_params(left=False, bottom=False) # Remove ticks
     sns.despine(left=True, bottom=True) # Remove borders borders
 
-    if lbl == 'y':
+# display label to bars based on plot orientation and label type
+    if y == None and lbl == 'y':
         for patch in ax.patches:
             count = patch.get_height()
             percentage = (count / len(df)) * 100
@@ -138,5 +150,18 @@ def ctplt(df, x:str, w, h, lim,xrot, lbl:str, lbl_type:str):
             x = patch.get_x() + patch.get_width() / 2 # Place the label at the top center of the bar
             y = patch.get_height() + 0.1  # Adjust y position for better placement
             ax.text(x, y, label, ha='center', va='bottom')
+    elif x == None and lbl == 'y':
+        for patch in ax.patches:
+            count = patch.get_width()
+            percentage = (count / len(df)) * 100
+            if lbl_type == '%':
+                label = f"{percentage:.1f}%"  # Format as percentage with one decimal
+            elif lbl_type == '#':
+                label = f"{count:.0f}"  # Format as percentage with one decimal
+            else:
+                label=None   
+            x = patch.get_x() + patch.get_width() +.1 
+            y = patch.get_y() + patch.get_height() /2 
+            ax.text(x, y, label, ha='left', va='center')
     else:
         label = None
